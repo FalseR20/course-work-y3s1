e4d1c94b724e587f6bda3fa6ac0cf951a6a058e0
FalseR20 <kno3.2002@gmail.com>
2022-11-10 19:48:53 +0300

    refactor

--- src/parser.py
+++ src/parser.py
@@ -1,3 +1,28 @@
+import datetime
+from typing import List, Tuple
+
 from coinbase.wallet.client import Client
 
 client = Client("api_key", "api_secret")  # key and secret are not required for prices parsing
+
+
+def parse_dataset(currency_pair: str) -> Tuple[List[float], List[float]]:
+    # prices = client.get_historic_prices(currency_pair=currency_pair, period="hour")["prices"]
+    # dataset: List[float] = [float(price["price"]) for price in prices]
+    # dataset_datetimes = [datetime.datetime.strptime(price["time"], "%Y-%m-%dT%H:%M:%SZ") for price in prices]
+    # last_time = dataset_datetimes[-1]
+    # dataset_times: List[float] = [(last_time - dataset_dt).total_seconds() / 60 for dataset_dt in dataset_datetimes]
+
+    # import pickle
+    # from src.data import _DATA_PATH
+    # with _DATA_PATH.joinpath("data.pkl").open("wb") as file:
+    #     pickle.dump((dataset, dataset_times), file)
+
+    import pickle
+
+    from src.data import _DATA_PATH
+
+    with _DATA_PATH.joinpath("data.pkl").open("rb") as file:
+        (dataset, dataset_times) = pickle.load(file)
+
+    return dataset, dataset_times
--- src/qt/main_window.py
+++ src/qt/main_window.py
@@ -6,7 +6,7 @@ import pyqtgraph as pg
 from PyQt6 import QtCore, QtGui, QtWidgets
 
 from src.data import BASE_CURRENCIES, CRYPTO_CURRENCIES
-from src.parser import client
+from src.parser import parse_dataset
 
 
 class MainWindow(QtWidgets.QMainWindow):
@@ -16,68 +16,80 @@ class MainWindow(QtWidgets.QMainWindow):
 
         # Main window
         self.setWindowTitle("Crypto-predictor")
-        self.setMinimumSize(300, 180)
-        self.resize(1000, 500)
+        self.setMinimumSize(600, 400)
+        self.resize(900, 500)
 
         # Central widget
         self.central_widget = QtWidgets.QWidget()
         self.setCentralWidget(self.central_widget)
-        self.central_widget_layout = QtWidgets.QVBoxLayout(self.central_widget)
 
-        # Currencies
+        # Currencies group
         self.currencies_group = QtWidgets.QGroupBox()
         self.currencies_group.setStyleSheet("QComboBox { combobox-popup: 0; }")
         self.currencies_group.setSizePolicy(QtWidgets.QSizePolicy.Policy.Minimum, QtWidgets.QSizePolicy.Policy.Maximum)
-        self.currencies_layout = QtWidgets.QHBoxLayout(self.currencies_group)
-        self.central_widget_layout.addWidget(self.currencies_group)
 
-        # Crypto currency combobox
-        self.crypto_currency = QtWidgets.QComboBox()
-        self.crypto_currency.setMaxVisibleItems(30)
-        self.currencies_layout.addWidget(self.crypto_currency)
+        # Currencies comboboxes
+        self.fiat_curr = QtWidgets.QComboBox(self.currencies_group)
+        self.crypto_curr = QtWidgets.QComboBox(self.currencies_group)
+        self.fiat_curr.setMaxVisibleItems(30)
+        self.crypto_curr.setMaxVisibleItems(30)
 
-        # Label between comboboxes '/'
-        label = QtWidgets.QLabel(" / ")
-        label.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
-        label.setSizePolicy(QtWidgets.QSizePolicy.Policy.Maximum, QtWidgets.QSizePolicy.Policy.Maximum)
-        self.currencies_layout.addWidget(label)
-
-        # Base currency combobox
-        self.base_currency = QtWidgets.QComboBox()
-        self.base_currency.setMaxVisibleItems(30)
-        self.currencies_layout.addWidget(self.base_currency)
+        # Slash-label between comboboxes
+        slash_label = QtWidgets.QLabel("  /  ", self.currencies_group)
+        slash_label.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
+        slash_label.setMinimumWidth(30)
+        slash_label.setSizePolicy(QtWidgets.QSizePolicy.Policy.Maximum, QtWidgets.QSizePolicy.Policy.Minimum)
 
         # Graph
         self.graph_item = pg.PlotItem()
+        self.graph_item.setLabel("left", "Price")
+        self.graph_item.setLabel("bottom", "Minutes")
         self.graph = pg.PlotWidget(self.central_widget, plotItem=self.graph_item)
         self.graph.setBackground(self.palette().color(QtGui.QPalette.ColorRole.Window))
-        self.central_widget_layout.addWidget(self.graph)
+        self.graph_item.addLegend()
 
         # Control panel
         self.control_group = QtWidgets.QGroupBox()
-        self.control_group.setSizePolicy(QtWidgets.QSizePolicy.Policy.Minimum, QtWidgets.QSizePolicy.Policy.Maximum)
-        self.control_group_layout = QtWidgets.QHBoxLayout(self.control_group)
-        self.central_widget_layout.addWidget(self.control_group)
+        self.control_group.setMaximumHeight(60)
 
         # Control run
         self.control_run = QtWidgets.QPushButton()
         self.control_run.setText("Run")
+        self.control_run.setSizePolicy(QtWidgets.QSizePolicy.Policy.Minimum, QtWidgets.QSizePolicy.Policy.Expanding)
         self.control_run.setCheckable(True)
         self.control_run.clicked.connect(self._control_checkbox_run_event)
-        self.control_group_layout.addWidget(self.control_run)
 
         # Control learn
         self.control_learn = QtWidgets.QPushButton()
         self.control_learn.setText("Learn")
-        self.control_learn.setCheckable(True)
+        self.control_learn.setSizePolicy(QtWidgets.QSizePolicy.Policy.Minimum, QtWidgets.QSizePolicy.Policy.Expanding)
         self.control_learn.setEnabled(False)
-        self.control_group_layout.addWidget(self.control_learn)
+        self.control_learn.clicked.connect(self._control_learn_event)
 
         # Control predicate
         self.control_predicate = QtWidgets.QPushButton()
         self.control_predicate.setText("Predicate")
+        self.control_predicate.setSizePolicy(
+            QtWidgets.QSizePolicy.Policy.Minimum, QtWidgets.QSizePolicy.Policy.Expanding
+        )
         self.control_predicate.setCheckable(True)
         self.control_predicate.setEnabled(False)
+        self.control_predicate.clicked.connect(self._control_predicate_event)
+
+        # Layouts
+        self.central_widget_layout = QtWidgets.QVBoxLayout(self.central_widget)
+        self.central_widget_layout.addWidget(self.currencies_group)
+        self.central_widget_layout.addWidget(self.graph)
+        self.central_widget_layout.addWidget(self.control_group)
+
+        self.currencies_layout = QtWidgets.QHBoxLayout(self.currencies_group)
+        self.currencies_layout.addWidget(self.crypto_curr)
+        self.currencies_layout.addWidget(slash_label)
+        self.currencies_layout.addWidget(self.fiat_curr)
+
+        self.control_group_layout = QtWidgets.QHBoxLayout(self.control_group)
+        self.control_group_layout.addWidget(self.control_run)
+        self.control_group_layout.addWidget(self.control_learn)
         self.control_group_layout.addWidget(self.control_predicate)
 
         # Final logic
@@ -85,32 +97,29 @@ class MainWindow(QtWidgets.QMainWindow):
 
     def _fill_currencies_comboboxes(self) -> None:
         for currency in BASE_CURRENCIES:
-            self.base_currency.addItem(currency)
-        self.base_currency.setCurrentText("USD")
+            self.fiat_curr.addItem(currency)
+        self.fiat_curr.setCurrentText("USD")
 
         for currency in CRYPTO_CURRENCIES:
-            self.crypto_currency.addItem(currency)
-        self.crypto_currency.setCurrentText("BTC")
+            self.crypto_curr.addItem(currency)
+        self.crypto_curr.setCurrentText("BTC")
 
     def _control_checkbox_run_event(self, is_checked: bool) -> None:
         self.log(f"self._control_checkbox_run_event({is_checked=})")
         self.currencies_group.setEnabled(not is_checked)
         self.control_learn.setEnabled(is_checked)
-        self.control_predicate.setEnabled(is_checked)
+
         if not is_checked:
+            self.graph_item.clear()
             return
 
-        self.log(f"{self.crypto_currency.currentText()}-{self.base_currency.currentText()}")
-        prices = client.get_historic_prices(
-            currency_pair=f"{self.crypto_currency.currentText()}-{self.base_currency.currentText()}",
-            period="hour",
-        )["prices"]
-        dataset: List[float] = [float(price["price"]) for price in prices]
-        last_time = datetime.datetime.strptime(prices[-1]["time"], "%Y-%m-%dT%H:%M:%SZ")
-        dataset_times = [
-            (last_time - datetime.datetime.strptime(price["time"], "%Y-%m-%dT%H:%M:%SZ")).total_seconds() / 60
-            for price in prices
-        ]
-        self.graph_item.clear()
-        # self.graph_item.addItem(pg.PlotDataItem({"price": dataset_times, "minutes": dataset}))
-        self.graph_item.addItem(pg.PlotDataItem(dataset_times, dataset))
+        dataset, dataset_times = parse_dataset(f"{self.crypto_curr.currentText()}-{self.fiat_curr.currentText()}")
+        item = pg.PlotDataItem(dataset_times, dataset, name="Actual price")
+        self.graph_item.addItem(item)
+
+    def _control_learn_event(self) -> None:
+        self.control_learn.setEnabled(False)
+        self.control_predicate.setEnabled(True)
+
+    def _control_predicate_event(self, b):
+        pass
