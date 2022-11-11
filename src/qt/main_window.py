b441ef89ea98284689e91923c60640e28f62e93c
FalseR20 <kno3.2002@gmail.com>
2022-11-11 12:03:35 +0300

    refactor: update plotting logic

--- src/qt/main_window.py
+++ src/qt/main_window.py
@@ -41,13 +41,23 @@ class MainWindow(QtWidgets.QMainWindow):
         slash_label.setMinimumWidth(30)
         slash_label.setSizePolicy(QtWidgets.QSizePolicy.Policy.Maximum, QtWidgets.QSizePolicy.Policy.Minimum)
 
-        # Graph
-        self.graph_item = pg.PlotItem()
-        self.graph_item.setLabel("left", "Price")
-        self.graph_item.setLabel("bottom", "Minutes")
-        self.graph = pg.PlotWidget(self.central_widget, plotItem=self.graph_item)
-        self.graph.setBackground(self.palette().color(QtGui.QPalette.ColorRole.Window))
-        self.graph_item.addLegend()
+        # Plot widget and item
+        self.plot_item = pg.PlotItem()
+        self.plot_item.setLabel("left", "Price")
+        self.plot_item.setLabel("bottom", "Minutes")
+        self.plot_item.addLegend()
+        self.plot_widget = pg.PlotWidget(self.central_widget, plotItem=self.plot_item)
+        self.plot_widget.setBackground(self.palette().color(QtGui.QPalette.ColorRole.Window))
+
+        # Plot data items
+        self.actual_price_data_item = pg.PlotDataItem(name="Actual price")
+        self.predicated_price_data_item = pg.PlotDataItem(name="Predicated price")
+        self.predicated_price_data_item.setPen(pg.mkPen(QtGui.QColor("red")))
+        self.updated_price_data_item = pg.PlotDataItem(name="Updated price")
+        self.updated_price_data_item.setPen(pg.mkPen(QtGui.QColor("blue")))
+        self.plot_item.addItem(self.actual_price_data_item)
+        self.plot_item.addItem(self.predicated_price_data_item)
+        self.plot_item.addItem(self.updated_price_data_item)
 
         # Control panel
         self.control_group = QtWidgets.QGroupBox()
@@ -58,7 +68,7 @@ class MainWindow(QtWidgets.QMainWindow):
         self.control_run.setText("Run")
         self.control_run.setSizePolicy(QtWidgets.QSizePolicy.Policy.Minimum, QtWidgets.QSizePolicy.Policy.Expanding)
         self.control_run.setCheckable(True)
-        self.control_run.clicked.connect(self._control_checkbox_run_event)
+        self.control_run.clicked.connect(self._control_run_event)
 
         # Control learn
         self.control_learn = QtWidgets.QPushButton()
@@ -86,7 +96,7 @@ class MainWindow(QtWidgets.QMainWindow):
         # Layouts
         self.central_widget_layout = QtWidgets.QVBoxLayout(self.central_widget)
         self.central_widget_layout.addWidget(self.currencies_group)
-        self.central_widget_layout.addWidget(self.graph)
+        self.central_widget_layout.addWidget(self.plot_widget)
         self.central_widget_layout.addWidget(self.control_group)
 
         self.currencies_layout = QtWidgets.QHBoxLayout(self.currencies_group)
@@ -112,49 +122,50 @@ class MainWindow(QtWidgets.QMainWindow):
             self.crypto_curr.addItem(currency)
         self.crypto_curr.setCurrentText("BTC")
 
-    def _control_checkbox_run_event(self, is_checked: bool) -> None:
-        self.log(f"control_checkbox_run_event: started with {is_checked=})")
+    def _control_run_event(self, is_checked: bool) -> None:
+        self.log(f"control_run_event: started with {is_checked=})")
         self.currencies_group.setDisabled(is_checked)
         self.control_learn.setEnabled(is_checked)
+        self.control_update.setEnabled(is_checked)
 
         if not is_checked:
-            self.graph_item.clear()
+            for data_item in self.plot_item.listDataItems():
+                data_item: pg.PlotDataItem
+                data_item.setData()
             return
 
         self.rates, self.datetimes = parse_dataset(f"{self.crypto_curr.currentText()}-{self.fiat_curr.currentText()}")
         self.last_time = self.datetimes[-1]
         self.times: List[float] = [(dataset_dt - self.last_time).total_seconds() / 60 for dataset_dt in self.datetimes]
         self.period_minutes = abs(self.times[-2])
-        item = pg.PlotDataItem(self.times, self.rates, name="Actual price")
-        self.graph_item.addItem(item)
-        self.log(f"control_checkbox_run_event: set params {self.last_time=}, {self.period_minutes=}, {self.times=}")
+        self.actual_price_data_item.setData(self.times, self.rates)
+        self.log(f"control_run_event: set params {self.last_time=}, {self.period_minutes=}, {self.times=}")
 
     def _control_learn_event(self) -> None:
         self.log("control_learn_event: started")
         learn(self.rates, 300)
         self.control_predicate.setEnabled(True)
+        self.log("control_learn_event: finished")
 
     def _control_predicate_event(self) -> None:
         n = 20
         self.log(f"control_learn_event: started with {n=}")
         data = predicate(self.rates, n)
         times = [i * self.period_minutes for i in range(0, n + 1)]
-        item = pg.PlotDataItem(times, data, name="Predicated price")
-        item.setPen(pg.mkPen(QtGui.QColor("red")))
-        self.graph_item.addItem(item)
+
+        self.predicated_price_data_item.setData(times, data)
         self.control_predicate.setDisabled(True)
-        self.control_update.setEnabled(True)
-        self.control_learn.setDisabled(True)
 
     def _control_update_event(self) -> None:
         self.log("control_update_event: started")
         rates, datetimes = parse_dataset(f"{self.crypto_curr.currentText()}-{self.fiat_curr.currentText()}")
-        last_i = datetimes.index(self.last_time)
+        try:
+            last_i = datetimes.index(self.last_time)
+        except ValueError:
+            self.log("control_update_event: No last_time in parsed data")
+            return
         rates = rates[last_i:]
         datetimes = datetimes[last_i:]
-        self.log(f"control_update_event: update data {datetimes=}")
+        self.log(f"control_update_event: update {len(datetimes) - 1} new data {datetimes=}")
         times: List[float] = [(dataset_dt - self.last_time).total_seconds() / 60 for dataset_dt in datetimes]
-        item = pg.PlotDataItem(times, rates, name="Updated price")
-        item.setPen(pg.mkPen(QtGui.QColor("blue")))
-        self.graph_item.addItem(item)
-        # self.control_update.setDisabled(True)
+        self.updated_price_data_item.setData(times, rates)
